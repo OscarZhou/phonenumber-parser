ed1e6a88272a750b87af1cce6f3343980d1c7b43
OscarZhou <zhouhongyu1989@gmail.com>
2018-09-05 23:43:45 +1200

    Refact the struct followed by E.164

--- parser/parser.go
+++ parser/parser.go
@@ -2,59 +2,73 @@ package parser
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 )
 
-type PhoneNumber struct {
-	Original    string
-	Formatted   string
-	CountryCode string
-	AreaCode    string
+// E164 follows the E.164 standard
+// When calling a number, country code should be prefixed
+//
+type E164 struct {
+	CountryCode    string // 64
+	TruckPrefixes  string // 0
+	AreaCode       string // 21
+	PhoneNumber    string // 1376664
+	DailingNumber  string // CountryCode + AreaCode + PhoneNumber
+	DomesticNumber string // TruckPrefixes + PhoneNumber
+	Alpha2         string // NZ
+	Alpha3         string // NZL
 }
 
-// func ParsePhoneNumberWithCountryCode(phoneNumber, countryCode string) (formmated string, err error) {
-
-// }
-
-// func ParsePhoneNumberWithAreaCode(phoneNumber, countryCode string) (formatted string, err error) {
-
-// }
-
-// func ParsePhoneNumber2CountryCode(phoneNumber string) (countryCode string, err error) {
-
-// }
-
-func ParsePhoneNumber(phoneNumber string) (*PhoneNumber, error) {
-	var entity PhoneNumber
+func ParsePhoneNumber(phoneNumber string) (E164, error) {
+	var entity E164
 	if phoneNumber == "" {
-		return nil, errors.New("phone number is required")
+		return E164{}, errors.New("phone number is required")
 	}
 
-	entity.Original = phoneNumber
+	tmpPhoneNumber := strings.Replace(phoneNumber, " ", "", -1)
+	tmpPhoneNumber = strings.Replace(tmpPhoneNumber, "(", "", -1)
+	tmpPhoneNumber = strings.Replace(tmpPhoneNumber, ")", "", -1)
+	tmpPhoneNumber = strings.Replace(tmpPhoneNumber, "-", "", -1)
+	tmpPhoneNumber = strings.Replace(tmpPhoneNumber, "+", "", -1)
 	iso3166 := GetISO3166()
+	current := ISO3166{}
 	for _, v := range iso3166 {
-		pattern := strings.Replace(phoneNumber, "+", "", -1)
-		if regexp.MustCompile(`\A` + v.CountryCode + `[\d*]`).MatchString(pattern) {
-			fmt.Printf("pattern is %s\n", pattern)
-			pattern = strings.Replace(pattern, v.CountryCode, "", 1)
-			fmt.Printf("pattern is %s\n", pattern)
-			for _, mobileCode := range v.MobileBeginWith {
-				if regexp.MustCompile(`\A0?` + mobileCode).MatchString(pattern) {
-					entity.CountryCode = v.Alpha2
-					entity.AreaCode = v.CountryCode
-					if pattern[0] == '0' {
-						entity.Formatted = "+" + v.CountryCode + pattern[1:]
-					} else {
-						entity.Formatted = "+" + v.CountryCode + pattern
-					}
-					fmt.Printf("formatted is %s\n", entity.Formatted)
-					break
-				}
-			}
+		if regexp.MustCompile(`\A` + v.CountryCode + `[\d*]`).MatchString(tmpPhoneNumber) {
+			entity.CountryCode = v.CountryCode
+			entity.Alpha2 = v.Alpha2
+			entity.Alpha3 = v.Alpha3
+			tmpPhoneNumber = strings.Replace(tmpPhoneNumber, v.CountryCode, "", 1)
+			truckCode := filterTruckCode(entity.Alpha2)
+			entity.TruckPrefixes = truckCode
+			tmpPhoneNumber = tmpPhoneNumber[len(truckCode):]
+			current = v
+			break
 		}
 	}
 
-	return &entity, nil
+	if current.CountryCode != "" {
+		for _, areaCode := range current.MobileBeginWith {
+			if strings.HasPrefix(tmpPhoneNumber, areaCode) {
+				entity.AreaCode = areaCode
+				tmpPhoneNumber = strings.Replace(tmpPhoneNumber, areaCode, "", 1)
+				entity.PhoneNumber = tmpPhoneNumber
+				entity.DailingNumber = entity.CountryCode + entity.AreaCode + entity.PhoneNumber
+				entity.DomesticNumber = entity.TruckPrefixes + entity.PhoneNumber
+				break
+			}
+		}
+	}
+	return entity, nil
+}
+
+func filterTruckCode(country string) string {
+	truckCode := ""
+	switch country {
+	case "MX":
+		truckCode = "01"
+	case "AZ", "RU", "KZ", "TC", "UZ", "BY":
+		truckCode = "8"
+	}
+	return truckCode
 }
